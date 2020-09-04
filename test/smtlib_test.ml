c4144901fe0414b917c1a8de253a0fe57c919e98
Geoff Hulette <geoff@hulette.net>
2020-09-03 19:42:41 -0700

    A test

--- test/smtlib_test.ml
+++ test/smtlib_test.ml
@@ -1,4 +1,17 @@
 open Easy_smt.Smtlib
+open Printf
+
+let run_test1 () =
+  let solv = make_solver "z3" in
+  let x = const "x" in
+  let y = const "y" in
+  assert_ solv (equals x y);
+  assert_ solv (equals x (int_to_term 5));
+  assert_ solv (equals y (add x (int_to_term 1)));
+  match check_sat solv with
+  | Sat -> printf "sat\n"
+  | Unsat -> printf "unsat\n"
+  | Unknown -> printf "unknown\n"
 
 let () =
-  print_endline (sexp_to_string (SList []));
+  run_test1 ()
